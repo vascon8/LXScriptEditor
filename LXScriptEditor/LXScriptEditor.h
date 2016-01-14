33b4969c408852ed01d69b3640ab29670852f875
SnowLiu <309122873@qq.com>
2016-01-14 10:30:08 +0800

    Initial Commit

---
+++ LXScriptEditor/LXScriptEditor.h
@@ -0,0 +1,13 @@
+//
+//  LXScriptEditor.h
+//  LXScriptEditor
+//
+//  Created by xinliu on 16-1-14.
+//  Copyright (c) 2016年 xinliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LXScriptEditor : NSObject
+
+@end
---
+++ LXScriptEditor/LXScriptEditor.m
@@ -0,0 +1,13 @@
+//
+//  LXScriptEditor.m
+//  LXScriptEditor
+//
+//  Created by xinliu on 16-1-14.
+//  Copyright (c) 2016年 xinliu. All rights reserved.
+//
+
+#import "LXScriptEditor.h"
+
+@implementation LXScriptEditor
+
+@end
